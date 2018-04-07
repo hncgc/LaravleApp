824a22166c741908391cbd10f3c9788e19a666ab
chenggongchun <cgc@prosysoft.com>
2018-04-07 18:23:08 +0800

    update

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'content', 'published_at'];
 }
